265b7fe0769b1e5ce0cff37bd3fa21a1fe9e7ee5
Uuclear <Uuclear@users.noreply.github.com>
2026-06-15 18:07:39 +0800

    Pure C# branch: remove Python web app, add build script and user guide.

--- ring_knife/calc/__init__.py
+++
@@ -1,3 +0,0 @@
-from ring_knife.calc.ring_knife import calculate_all, calculate_point
-
-__all__ = ["calculate_all", "calculate_point"]
--- ring_knife/report/__init__.py
+++
@@ -1,3 +0,0 @@
-from ring_knife.report.docx_generator import generate_report
-
-__all__ = ["generate_report"]
